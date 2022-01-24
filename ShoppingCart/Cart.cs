a0dc8f28a81d8831623db315ba221da85fb6f827
Malek Dekhil <malekdekhil68@gmail.com>
2022-01-24 09:27:16 +0100

    delete TotalItems

--- ShoppingCart/Cart.cs
+++ ShoppingCart/Cart.cs
@@ -4,18 +4,12 @@ namespace ShoppingCart
 {
     public class Cart
     {
-
         public List<CartItem> ListProducts { get; set; }
-
         [Column(TypeName = "decimal(8,2)")]
         public decimal Total { get; set; }
-
-        [Column(TypeName = "decimal(8,2)")]
-        public decimal TotalAllItems { get; set; }
         public Cart()
         {
             ListProducts = new List<CartItem>();
         }
-
     }
 }
