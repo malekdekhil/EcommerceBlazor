a2ea17d086d7f3377412bca8649c46ec151cca29
Malek Dekhil <malekdekhil68@gmail.com>
2022-01-30 16:56:09 +0100

    Services_ClsProduct_Tests TU  4/10

--- Noums_T_U/Services_ClsProduct_Tests.cs
+++ Noums_T_U/Services_ClsProduct_Tests.cs
@@ -3,6 +3,7 @@ using Domains;
 using FluentAssertions;
 using Moq;
 using Services.Implementations;
+using Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace Noums_T_U
             //A
             sut.Should().NotBeNullOrEmpty();
             sut.Should().BeOfType<List<Product>>();
-            foreach (var product in sut.Where(a=>a.IdProduct == idProduct))
+            foreach (var product in sut.Where(a => a.IdProduct == idProduct))
             {
                 product.Category.CategoryName.Should().Be(category);
                 product.IdCategory_Fk.Should().Be(idCategory);
@@ -41,7 +42,6 @@ namespace Noums_T_U
                 product.IdProduct.Should().Be(idProduct);
             }
         }
-
         [Theory]
         [InlineAutoData(1, 1, "product1", 5, 15, "picture1", "path\\picture1", 1)]
         private async void Retrun_Products_Include_Pictures_GetAllProductsAsync_Test(
@@ -71,6 +71,36 @@ namespace Noums_T_U
                     pic.IdPictures.Should().Be(idPicture);
                 }
             }
+        }
+        [Theory]
+        [AutoData]
+        private async void Verify_Executing_Of_Update_UpdateProductAsync_Test([Frozen] Mock<IProduct> iproduct, [Frozen] Mock<IUnitOfWork> unit)
+        {
+            var currentProd = GetProductByIdIncludeCategories(1);
+            var newProd = GetProductByIdIncludeCategories(2);
+            //A
+            iproduct.Setup(layer => layer.UpdateProductAsync(currentProd, newProd)).Verifiable();
+            //A
+            await new ClsProduct(unit.Object).UpdateProductAsync(currentProd, newProd);
+            //A
+            unit.Verify(a => a.CommitAsync(), Times.Once);
+        }
+        [Theory]
+        [AutoData]
+        private async void Verify_Delete_Product_RemoveProductAsync_Test([Frozen] Mock<IProduct> iproduct, [Frozen] Mock<IUnitOfWork> unit)
+        {
+            var currentProd = GetProductByIdIncludeCategories(1);
+            //A
+            iproduct.Setup(l => l.RemoveProductAsync(currentProd)).Verifiable();
+            //A
+            await new ClsProduct(unit.Object).RemoveProductAsync(currentProd);
+            //A
+            unit.Verify(a => a.Products.Remove(currentProd), Times.Once);
+            unit.Verify(a => a.CommitAsync(), Times.Once);
+        }
+        private Product GetProductByIdIncludeCategories(int id)
+        {
+            return ProductsIncludeCategories().Result.Where(a => a.IdProduct == id).FirstOrDefault();
         }
         private async Task<IEnumerable<Product>> ProductsIncludeCategories()
         {
